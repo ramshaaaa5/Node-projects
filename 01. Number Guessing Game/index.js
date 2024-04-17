192359c6476de1013ff0c3e6820c615a20e443cd
Ramsha Shoaib <159899975+ramshaaaa5@users.noreply.github.com>
2024-04-17 19:53:04 +0500

    Add files via upload

---
+++ 01. Number Guessing Game/index.js
@@ -0,0 +1,20 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+let condition = true;
+while (condition) {
+    const randomNumber = Math.floor(Math.random() * 4 + 1);
+    const answers = await inquirer.prompt([
+        {
+            name: "userGuessedNumber",
+            type: "number",
+            message: "Please guess a number between 1-4:",
+        },
+    ]);
+    if (answers.userGuessedNumber === randomNumber) {
+        console.log("Congratulations! You guessed right number.");
+    }
+    else {
+        console.log("You guessed wrong number!");
+    }
+    console.log(answers);
+}
